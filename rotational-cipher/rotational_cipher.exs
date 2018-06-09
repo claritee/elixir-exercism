5c8ebff8aa1f6475e3582e50057e23d1224c13e0
Claire Tran <c.tran83@gmail.com>
2018-06-09 16:35:58 +1000

    updating solution

--- rotational-cipher/rotational_cipher.exs
+++ rotational-cipher/rotational_cipher.exs
@@ -8,5 +8,19 @@ defmodule RotationalCipher do
   """
   @spec rotate(text :: String.t(), shift :: integer) :: String.t()
   def rotate(text, shift) do
+  	text
+  	|> String.to_charlist
+  	|> Enum.map(&(new_value(&1, shift)))
+  	|> String.Chars.to_string
+  end
+
+  def new_value(x, shift) do
+  	cond do
+		(x in ?a..?z) && (x + shift <= ?z) -> x + shift	
+		(x in ?a..?z) && (x + shift > ?z) -> x - 26 + shift
+		(x in ?A..?Z) && (x + shift <= ?Z) -> x + shift
+		(x in ?A..?Z) && (x + shift > ?Z) -> x - 26 + shift
+		true -> x
+	end
   end
 end
